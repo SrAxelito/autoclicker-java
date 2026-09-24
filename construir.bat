9b1570101d094d284e28eb5eff9bbf20e6d2df30
Alexander Fajardo <alexander.fajardo0919@gmail.com>
2026-09-24 18:35:49 -0500

    build: agregar JNativeHook y adaptar el script de compilación

--- construir.bat
+++ construir.bat
@@ -1,15 +1,21 @@
 @echo off
+setlocal
+set JNH=jnativehook-2.2.2.jar
+
 echo Limpiando versiones anteriores...
 if exist out rmdir /s /q out
 if exist app rmdir /s /q app
 if exist salida rmdir /s /q salida
 
 echo Compilando...
-javac -d out -sourcepath src src\autoclicker\Main.java || goto error
+javac -encoding UTF-8 -cp lib\%JNH% -d out -sourcepath src src\autoclicker\Main.java || goto error
 
 echo Empaquetando .jar...
 mkdir app
-jar cfe app\AutoClicker.jar autoclicker.Main -C out . || goto error
+> out\MANIFEST.MF echo Main-Class: autoclicker.Main
+>> out\MANIFEST.MF echo Class-Path: %JNH%
+jar cfm app\AutoClicker.jar out\MANIFEST.MF -C out autoclicker || goto error
+copy /y lib\%JNH% app\ >nul || goto error
 
 echo Generando el .exe...
 jpackage --type app-image --input app --main-jar AutoClicker.jar --name AutoClicker --dest salida || goto error
